84132a17306aefafd4140e62351ad0b4af7322df
chen__000 <chen__000@172.20.41.193>
2015-10-01 14:33:37 +0200

    creado el proyecto

--- tutorial1/src/tutorial1/Tutorial1.java
+++ tutorial1/src/tutorial1/Tutorial1.java
@@ -18,5 +18,6 @@ public class Tutorial1 {
         // TODO code application logic here
         System.out.append("hola mundo");
         System.out.println("segunda linea");
+        System.out.println("aniadido desde la rama");
     
 }
